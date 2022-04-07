4b147035a229be32d4ae5ae8949a69b3c9e2f61b
Christian <you@example.com>
2022-04-07 03:07:46 -0500

    cambios

--- config/db.php
+++ config/db.php
@@ -1,18 +1,17 @@
 <?php
 /**/
 return [
-
-  'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=ec2-54-157-79-121.compute-1.amazonaws.com;dbname=dfn3m087fsipl',
-    'username' => 'rwrphelswemffi',
-    'password' => '434801de2d2ed6e93f7674f6b2d68d4f8e7e2820851db7aa2cb97069ae9f2ae4',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=tre1',
+    'username' => 'postgres',
+    'password' => 'barcelona97.',
     'charset' => 'utf8',
-
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'schemaMap' => [
+        'pgsql'=> [
+            'class'=>'yii\db\pgsql\Schema',
+            'defaultSchema' => 'public'
+        ]
+    ],
 ];
 
 /*
