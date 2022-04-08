de5f2634e91ae1603b2d25f5321b8b05b8f32569
unknown <cdandrango@uce.edu.ec>
2022-04-08 00:31:11 -0500

    ver

--- config/db.php
+++ config/db.php
@@ -2,22 +2,12 @@
 /**/
 return [
 
-  //'class' => 'yii\db\Connection',
-    //'dsn' => 'pgsql:host=ec2-54-157-79-121.compute-1.amazonaws.com;dbname=dfn3m087fsipl',
-    //'username' => 'rwrphelswemffi',
-    //'password' => '434801de2d2ed6e93f7674f6b2d68d4f8e7e2820851db7aa2cb97069ae9f2ae4',
-    //'charset' => 'utf8',
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=tre1',
-    'username' => 'postgres',
-    'password' => 'yocrist1',
+  'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=ec2-54-157-79-121.compute-1.amazonaws.com;dbname=dfn3m087fsipl',
+    'username' => 'rwrphelswemffi',
+    'password' => '434801de2d2ed6e93f7674f6b2d68d4f8e7e2820851db7aa2cb97069ae9f2ae4',
     'charset' => 'utf8',
-    'schemaMap' => [
-        'pgsql'=> [
-            'class'=>'yii\db\pgsql\Schema',
-            'defaultSchema' => 'public'
-        ]
-    ],
+
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
